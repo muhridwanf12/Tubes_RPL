73c54765e1ffdf52b24c8cf3af327adc2cd09747
QisthiAH <qisthiah29@gmail.com>
2023-12-02 10:48:05 +0700

    slicing file

---
+++ lib/page_kalkulator/_index.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:tubes/home/home_data.dart';
+
+import 'hitung_usia.dart';
+
+part 'hasil_hitung/BB_TB.dart';
+part 'hasil_hitung/BB_U.dart';
+part 'hasil_hitung/IMT_U.dart';
+part 'hasil_hitung/TB_U.dart';
+part 'hasil_hitung/pewarnaan_nilai.dart';
+part 'kalkulator.dart';
